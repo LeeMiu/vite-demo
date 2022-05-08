312e1a967f639a5ab316aaf70fdf4f37e07a251d
libiao <libiao2014@xiaochuankeji.cn>
2022-05-08 14:27:28 +0800

    add ssr

---
+++ src/entry-server.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+export function render(url, context) {
+  return ReactDOMServer.renderToString(
+    <StaticRouter location={url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+}
